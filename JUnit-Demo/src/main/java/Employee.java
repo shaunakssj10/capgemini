9c9291c7f10dcc26875e8d8258eef751d13d0bb7
shaunakssj10 <60471157+shaunakssj10@users.noreply.github.com>
2020-02-04 15:33:05 +0530

    Add files via upload

---
+++ JUnit-Demo/src/main/java/Employee.java
@@ -0,0 +1,25 @@
+
+public class Employee {
+
+	private int id;
+	public Employee(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	private String name;
+	
+}
+
---
+++ JUnit-Demo/src/test/java/SampleTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SampleTest {
+
+	@Test
+	public void testPass() {
+		int a =1;
+		int b =2;
+		assertTrue(a+b == 3);
+	}
+	@Test
+	public void testFail() {
+		int a =1;
+		int b =2;
+		assertTrue(a+b == 4);
+	}
+
+}
